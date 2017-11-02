0dd2c221459655766dcad95f8ce80126092c46a1
Mehdi <mehdi@mehdi-aberkane.fr>
2017-11-02 10:43:03 +0100

    add exp

--- example.py
+++ example.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 import math as toto
+
+#Variable
 a = 12
 b = 6
+c = "salut les gens"
+
+# print in and or if del for is raise assert elif from lambda 
+# return break else global not try class except while continue exec import pass yield def finally
 
 print(toto.acosh(a + b))
+print(c)
 
+#Echapper les quotes
+txt = "Salut je m'appelle \"Asaku\""
+print(txt)
+
+#Condition
 if a < b:
 	print(a + b)
 else:
@@ -15,3 +27,4 @@ def maFunction(param1, param2='hihouuuu'):
 	print(param2)
 
 maFunction(1)
+
