ba82571d793d5d8a277e9e96b6bb9f49461f9233
Mehdi <mehdi@mehdi-aberkane.fr>
2018-05-08 02:27:31 +0200

    ip

--- Exercice/sudoku.py
+++ Exercice/sudoku.py
@@ -2,35 +2,41 @@
 # coding: utf-8
 
 import tkinter, random
+import sys
+sys.setrecursionlimit(8000)
 
 grid = []
+
+def generateNumber():
+    return random.randint(1, 9)
 
 def generateEmptyGrid():
     while len(grid) != 9:
         grid.append([0,0,0,0,0,0,0,0,0])
     return grid
 
-def checkNumber(number, position):
+# check if number is in columns
+def checkNumber(position, line):
+    number = generateNumber()
+    while line.count(number) > 0:
+        number = generateNumber()
     for l in grid:
         if l[position] == number:
-            return False
-    return True
+            checkNumber(position, line)
+
+    return number
 
 def generateGrid():
     grid = generateEmptyGrid()
     for line in grid:
         for idx, p in enumerate(line):
-            print("idx etape 1 ", idx)
-            number = random.randint(1, 9)
-            # check if number is in columns
-            print("idx etape 2 ", idx)
-            if checkNumber(number, idx):
-                line.append(number)
+            number = checkNumber(idx, line)
+            line[idx] = number
     return grid
 
 
 grid = generateGrid()
 
 
-# for l in grid:
-#     print(l)
+for l in grid:
+    print(l)
