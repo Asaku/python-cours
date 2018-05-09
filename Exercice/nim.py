d2bde0ed347ddeca60e0dba901142d7c09d424e3
Mehdi <mehdi@mehdi-aberkane.fr>
2018-05-09 07:18:20 +0200

    add new file

---
+++ Exercice/nim.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+stick = 21
+
+print("Welcome to the Nim game")
+
+print("Choose the number of player, 1 vs computeur or 1 vs 1")
+
+#choice = input("1 for computeur, 2 for two player")
+
+print(" | "*stick)
