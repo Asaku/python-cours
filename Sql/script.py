aa8c7a34a9e79dcc765b44d01d27caeb93452080
Mehdi <mehdi@mehdi-aberkane.fr>
2018-06-02 18:03:00 +0200

    add test

--- Sql/script.py
+++ Sql/script.py
@@ -1,2 +1,11 @@
-import mysqlclient
+import _mysql
+
+cnx = _mysql.connect(
+                        user='root',
+                        host='127.0.0.1',
+                        db='chevre'
+)
+cursor = cnx.cursor()
+
+print(cursor)
 
