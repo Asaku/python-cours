e362004e78b7245ebdd3bb822851713492720b83
Mehdi <mehdi@mehdi-aberkane.fr>
2020-01-17 17:48:27 +0100

    update ip

--- Scapy/script.py
+++ Scapy/script.py
@@ -5,12 +5,12 @@ from scapy.all import *
 
 
 #Ping
-mac = Ether() / IP(dst='192.168.1.27') / ICMP()
+mac = Ether() / IP(dst='192.168.1.50') / ICMP()
 
 rep,non_rep = srp(mac)
 
 #Sniff
 
-rep = sniff(filter="host 192.168.1.24")
+rep = sniff(filter="host 192.168.1.50")
 
 rep.show()
