e42577241f8955d3613a2f04bfc85ee0d6276812
Mehdi <mehdi@mehdi-aberkane.fr>
2020-01-18 19:10:21 +0100

    update python 2 to p3

--- Commander/client.py
+++ Commander/client.py
@@ -3,14 +3,14 @@
 
 import socket, sys
 
-TCP_IP = ""
+TCP_IP = "192.168.1.9"
 TCP_PORT = 15555
 BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(sys.argv[1])
+s.send(sys.argv[1].encode())
 data = s.recv(BUFFER_SIZE)
 s.close()
 
-print "received data:", data
+print ("received data:", data)
